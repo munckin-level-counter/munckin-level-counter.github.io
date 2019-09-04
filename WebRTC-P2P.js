e069aacf6e117f6b4abede6c5605fef9a9d17775
Purexo <contact@purexo.mom>
2019-09-04 16:59:19 +0200

    total Rework WebRTC

--- WebRTC-P2P.js
+++ WebRTC-P2P.js
@@ -1,148 +1,76 @@
-const CFG_CONNECTION = {
-  iceServers: [
-    {urls: ['stun:stun2.l.google.com:19302']},
-    {urls: ['stun:stun.stunprotocol.org:3478']},
-  ],
-};
+class WebRTCp2p {
+  constructor(displayOffer, getAnswer, getOffer, displayAnswer) {
+    this.peers = new Set();
 
-const peers = [];
-
-function uid() {
-  return Number(Math.random() * 100000000000000000).toString(36);
-}
-
-async function host({displayInvitation=console.log, provideResponse=Promise.reject(new Error('provideResponse not provided'))}={}) {
-  const peer = new RTCPeerConnection(CFG_CONNECTION);
-  window.peer = peer;
-
-  const channel = peer.createDataChannel('default');
-  const ping = peer.createDataChannel('ping');
-
-  peer.channels = new Map([
-    [channel.label, channel],
-    [ping.label, ping],
-  ]);
-
-  ping.addEventListener('message', event => console.log('SUCCESS, connection established !'));
-
-  channel.addEventListener('open', event => {
-    console.log(`channel ${channel.label} is ready`, event)
-  });
-
-  // dispatch message to other peers
-  channel.addEventListener('message', event => {
-    console.log('Got :', event.data);
-
-    peers.forEach(other => {
-      if (other === peer) return;
-
-      const otherChan = other.channels.get(channel.label);
-
-      otherChan && otherChan.send(event.data);
-    });
-  });
-
-  peer.addEventListener('icecandidate', event => {
-    if (event.candidate) return;
-
-    console.log('Invitation message :');
-    console.log(JSON.stringify(peer.localDescription));
-    
-    Promise.resolve(displayInvitation(peer.localDescription))
-      .then(confirmHandshake)
-      .catch(console.error);
-  });
-
-  // start handshake
-  const description = await peer.createOffer();
-  await peer.setLocalDescription(description);
-
-  async function confirmHandshake() {
-    try {
-      const answer = await provideResponse();
-      await peer.setRemoteDescription(answer);
-
-      peers.push(peer);
-
-      ping.send('ping');
-    } catch (error) {
-      console.error(error);
-
-      for (const channel of peer.channels.values()) {
-        channel.close()
-      }
-
-      return peer.close();
-    }
+    this.displayOffer = displayOffer;
+    this.getAnswer = getAnswer;
+    this.getOffer = getOffer;
+    this.displayAnswer = displayAnswer;
   }
-}
 
-async function join(offer, sendResponse) {
-  const peer = new RTCPeerConnection(CFG_CONNECTION);
-  window.peer = peer;
-  peer.channels = new Map();
+  async invite() {
+    const peer = new RTCPeerConnection({iceServers: [{urls: []}]});
+    peer.channels = new Map();
 
-  peer.addEventListener('datachannel', event => {
-    const channel = event.channel;
-    peer.channels.set(channel.label, channel);
+    let state = 0;
 
-    switch (event.channel.label) {
-      case 'default':
-        channel.addEventListener('open', event => {
-          console.log(`channel ${channel.label} is ready`, event)
-        });
+    async function handshake() {
+      if (state > 0 || state < 6) return;
 
-        // dispatch message to other peers
-        channel.addEventListener('message', event => {
-          console.log('Got :', event.data);
-
-          peers.forEach(other => {
-            if (other === peer) return;
-
-            const otherChan = other.channels.get(channel.label);
-
-            otherChan && otherChan.send(event.data);
-          });
-        });
-
-        break;
-      case 'ping':
-        channel.addEventListener('message', event => {
-          console.log('SUCCESS, connection established !');
-
-          channel.send('pong');
-        });
-        break;
-    }
-  });
-
-  peer.addEventListener('icecandidate', event => {
-    if (event.candidate) return;
-
-    console.log('Answer response :');
-    console.log(JSON.stringify(peer.localDescription));
-  
-    sendResponse(peer.localDescription);
-  });
-
-  try {
-    await peer.setRemoteDescription(new RTCSessionDescription(offer));
-    const description = await peer.createAnswer();
-    await peer.setLocalDescription(description);
-
-    peers.push(peer);
-  } catch (error) {
-    console.error(error);
-
-    for (const channel of peer.channels.values()) {
-      channel.close()
+      state = 3; await this.displayOffer(peer.localDescription);
+      state = 4; const answer = await this.getAnswer();
+      state = 5; await peer.setRemoteDescription(answer);
+      state = 6; console.log('OK all set');
     }
 
-    peer.close();
+    peer.addEventListener('datachannel', event => peer.channels.set(event.channel.label, event.channel));
+    peer.channels.set('data', peer.createDataChannel('data'));
+
+    peer.addEventListener('icecandidate', e => e.candidate || handshake().catch(console.error));
+
+    state = 1; const offer = await peer.createOffer();
+    state = 2; await peer.setLocalDescription(offer);
+
+    await handshake();
+
+    this.peers.add(peer);
+
+    return peer;
   }
-}
 
-function sendMessage(str) {
-  console.log('You :', str);
-  peers.forEach(peer => peer.channel.send(str));
+  async join() {
+    const peer = new RTCPeerConnection({iceServers: [{urls: []}]});
+    peer.channels = new Map();
+
+    let state = 0;
+
+    async function handshake() {
+      if (state > 0 || state < 6) return;
+
+      state = 5; await this.displayAnswer(peer.localDescription);
+      state = 6; console.log('OK all set');
+    }
+
+    peer.addEventListener('datachannel', event => peer.channels.set(event.channel.label, event.channel));
+
+    peer.addEventListener('icecandidate', e => e.candidate || handshake().catch(console.error));
+
+    state = 1; const offer = await this.getOffer();
+    state = 2; await peer.setRemoteDescription(offer);
+    state = 3; const answer = await peer.createAnswer();
+    state = 4; await peer.setLocalDescription(answer);
+
+    await handshake();
+
+    this.peers.add(peer);
+
+    return peer;
+  }
+
+  /**
+   * broadcast message to all peers
+   */
+  send(message, channel = 'data') {
+    this.peers.forEach(peer => peer.channels.get(channel).send(message))
+  }
 }
